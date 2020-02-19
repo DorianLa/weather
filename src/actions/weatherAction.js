9b2e38229cff66c310474226c52df35263ce5ce6
Dorian Labarre <dorian54300@hotmail.be>
2020-02-19 10:21:18 +0100

    feat(weather): Review weatherAction

--- src/actions/weatherAction.js
+++ src/actions/weatherAction.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 
 // requête Get de http://api.openweathermap.org/data/2.5/weather?q=London&appid=cc08a99a76962840a56efd9df1bfad13
 // Axios pour faire la requête
-
+const baseUrl = "http://api.openweathermap.org/data/2.5/";
+const appId = "&appid=cc08a99a76962840a56efd9df1bfad13";
 export function getWeather(){
-    return axios.get("http://api.openweathermap.org/data/2.5/weather?q=London&appid=cc08a99a76962840a56efd9df1bfad13")
+    return axios.get(baseUrl+"weather?q=London"+appId);
 }
 
